0d7b84452bbc97f3f41bf31fd273ae5dc7a8bb06
Quadri-Ian <104133443+Quadri-Ian@users.noreply.github.com>
2023-10-01 02:32:01 +0100

    second commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+
+module.exports = {
+    // Other configurations...
+    productionBrowserSourceMaps: true,
+    // Add any other Next.js configurations you need here.
+  };
+  
